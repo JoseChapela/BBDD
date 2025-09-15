66a47c3be099a771d031fe566c47e9c2de25eda1
JCP <jchappast@ieschandomonte.edu.es>
2025-09-15 20:33:34 +0200

    probando conexion

--- src/servicios/OperacionesIO.java
+++ src/servicios/OperacionesIO.java
@@ -39,35 +39,4 @@ public class OperacionesIO {
             System.out.println("-|"+filename + fileLastUpdate);
         }
     }
-
-    static void recorrerRecursivo (String ruta) {
-        File file = new File(ruta);
-
-        System.out.println("---LISTANDO EL DIRECTORIO "+ruta+"---");
-
-        if(!file.exists()){
-            System.out.println("La ruta especificada no existe");
-            return;
-        }
-        if(!file.isDirectory()){
-            System.out.println("La ruta no es un directorio");
-            return;
-        }
-
-        File[] files = file.listFiles();
-        String encabezado = "---|";
-
-        for(File f : files){
-            String filename, fileLastUpdate;
-
-            filename = f.getName();
-            if(f.isDirectory())
-                filename = filename+" <DIR> ";
-            else if(f.isFile()){
-                filename = filename+" <FICHERO> "+  String.format("%.3f", f.length()/1024.0) +"_KB ";
-            }
-            fileLastUpdate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(f.lastModified());
-            System.out.println("-|"+filename + fileLastUpdate);
-        }
-    }
 }
